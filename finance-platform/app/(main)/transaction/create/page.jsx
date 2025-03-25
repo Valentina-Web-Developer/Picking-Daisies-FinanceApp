645ef5aa5578f0065d2da710c1aa6fa9e339f282
Valentina-Web-Developer <valentina.webdev@gmail.com>
2025-03-25 11:18:28 +0100

    fixe dbug on db

--- finance-platform/app/(main)/transaction/create/page.jsx
+++ finance-platform/app/(main)/transaction/create/page.jsx
@@ -1,17 +1,53 @@
+// import { getUserAccounts } from "@/actions/dashboard";
+// import { defaultCategories } from "@/data/categorie";
+// import { AddTransactionForm } from "../_components/transactionForm";
+// import { getTransaction } from "@/actions/transaction";
+
+// const AddTransactionPage = async ({ searchParams }) => {
+//   const accounts = await getUserAccounts();
+//   const editId = searchParams?.edit;
+
+//   let initialData = null;
+//   if (editId) {
+//     const transaction = await getTransaction(editId);
+//     initialData = transaction;
+//   }
+
+//   return (
+//     <div className="max-w-3xl mx-auto px-5">
+//       <div className="flex justify-center md:justify-normal mb-8">
+//         <h1 className="text-5xl gradient-title ">
+//           {editId ? "Edit" : "Add"} Transaction
+//         </h1>
+//       </div>
+//       <AddTransactionForm
+//         accounts={accounts}
+//         categories={defaultCategories}
+//         editMode={!!editId}
+//         initialData={initialData}
+//       />
+//     </div>
+//   );
+// };
+
+// export default AddTransactionPage;
 import { getUserAccounts } from "@/actions/dashboard";
 import { defaultCategories } from "@/data/categorie";
 import { AddTransactionForm } from "../_components/transactionForm";
 import { getTransaction } from "@/actions/transaction";
 
 const AddTransactionPage = async ({ searchParams }) => {
-  const accounts = await getUserAccounts();
   const editId = searchParams?.edit;
 
-  let initialData = null;
-  if (editId) {
-    const transaction = await getTransaction(editId);
-    initialData = transaction;
-  }
+  const accountsPromise = getUserAccounts();
+  const transactionPromise = editId
+    ? getTransaction(editId)
+    : Promise.resolve(null);
+
+  const [accounts, initialData] = await Promise.all([
+    accountsPromise,
+    transactionPromise,
+  ]);
 
   return (
     <div className="max-w-3xl mx-auto px-5">
